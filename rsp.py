6a0bd1dada548d72650d1e58c6ffddc2f68722c2
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2022-11-13 13:31:36 +0900

    quit_numberを変更

--- rsp.py
+++ rsp.py
@@ -1,8 +1,8 @@
 import random
 comp = [1,2,3]
-quit_number = int()
+quit_string = "continue"
 string = str()
-while quit_number < 10:
+while quit_string == "continue":
   is_input_correct = string in ["rock","paper", "scissors"]
   if is_input_correct == False:
     string = input("only input rock, paper or scissors: ")
@@ -12,21 +12,20 @@ while quit_number < 10:
     dict_player = {"rock": 1, "paper": 2, "scissors": 3}
     player_hand = dict_player[string]
     dict_computer = {1:"rock", 2:"paper", 3:"scissors"}
-    test = player_hand - computer_hand
-    if test < 0:
-      test = test + 3         
-    if test == 0:
+    result = player_hand - computer_hand
+    if result < 0:
+      result = result + 3         
+    if result == 0:
       print("computer hand is:" + str(dict_computer[computer_hand]))
       print("draw")
-      quit_number = 5
+      quit_string = "continue"
       string = input("input rock, paper or scissors: ")
-    elif test == 1:
+    elif result == 1:
       print("computer hand is:" + str(dict_computer[computer_hand]))
       print("you win")
-      quit_number = 15
+      quit_string = "quit"
     else:               
       print("computer hand is:" + str(dict_computer[computer_hand]))
       print("you lose")
-      quit_number = 15
+      quit_string = "quit"
 print('End')
-# rock,paper,scissors 以外入力されたときを考える。if文を使う。
