5ecb1a8e4b02b4ce0d0d6418471c6e071babcccf
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2022-11-21 14:19:30 +0900

    手を出力

--- new-rsp.py
+++ new-rsp.py
@@ -1,3 +1,4 @@
-from util import get_player_hand, get_opponent_hand,convert_playerhand_to_number
-print(get_player_hand())
-print(convert_playerhand_to_number())
+from util import get_player_hand, get_opponent_hand,convert_playerhand_to_number,convert_oppnenthand_to_number,judge_win_or_lose
+
+# dict = {"rock": 1, "paper": 2, "scissors": 3}
+print(judge_win_or_lose())
--- util.py
+++ util.py
@@ -11,15 +11,35 @@ def get_player_hand():
 # playerの手を数値に変換
 def convert_playerhand_to_number():
   dict = {"rock": 1, "paper": 2, "scissors": 3}
-  get_number = dict.get(get_player_hand())
+  get_number = dict[get_player_hand()]
   return get_number
   
 # 敵の手を選ぶ
 def get_opponent_hand():
-  number = [1,2,3]
-  opponent_hand = random.choice(number)
+  random_str = ["rock", "paper", "scissors"]
+  opponent_hand = random.choice(random_str)
   return opponent_hand
+
+# 敵の手を数値に変換
+def convert_oppnenthand_to_number():
+  dict = {"rock": 1, "paper": 2, "scissors": 3}
+  get_oppnent_number = dict[get_opponent_hand()]
+  return get_oppnent_number
   
 # 勝利判定
-
+def judge_win_or_lose():
+  result =  convert_playerhand_to_number() - convert_oppnenthand_to_number()
+  if result < 0:
+    result = result + 3
+  if result == 0:
+    print("computer hand is:" + str(get_opponent_hand()))
+    print("draw")
+    get_player_hand()
+  elif result == 1:
+    print("computer hand is:" + str(get_opponent_hand()))
+    print("you win")
+  else:
+    print("computer hand is:" + str(get_opponent_hand()))
+    print("you lose")
+    
 # 結果の表示
