c744bc170381e6fded5b04b46b5d886ba5ad06fa
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2022-12-12 00:57:49 +0900

    modify

--- util.py
+++ util.py
@@ -18,7 +18,13 @@ def get_opponent_hand():
 
 
 def get_winner(player_hand, opponent_hand):
-  """ 勝利判定"""
+  """ 勝利判定
+  
+  Return:
+  winner(dic): じゃんけんの勝利判定の辞書
+  difference(int): じゃんけんの判定値
+  
+  """
   
   hand_num_table = {"rock": 1, "paper": 2, "scissors": 3}
   player = hand_num_table[player_hand]
@@ -27,19 +33,29 @@ def get_winner(player_hand, opponent_hand):
   winner = { 0: None , 1: "player", 2: "opponent"}
   return winner[difference]
 
-def display_result():
+def display_result(value):
     """結果の表示"""
-    
-    value = None  
-    while value == None:
-      player_hand = get_player_hand()
-      opponent_hand = get_opponent_hand()
-      print("your hand is :" + player_hand)
-      print("opponent hand is: " + opponent_hand)
-      value = get_winner(player_hand, opponent_hand)
-      if value == None:
-        print("draw")
+
+    if value == None:
+      print("draw")
     if value == "player":
       print("you win")
     if value == "opponent":
       print("you lose")
+
+
+def display_input():
+    """入力する手と相手の手を表示
+    
+    Ruturn:
+    hand_list(list): 自分と相手の手をリスト化
+    
+    """
+
+    player_hand = get_player_hand()
+    opponent_hand = get_opponent_hand()
+    hand_list = [player_hand, opponent_hand]
+    print("your hand is :" + player_hand)
+    print("opponent hand is: " + opponent_hand)
+    return hand_list
+    
