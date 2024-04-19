6ab1b0d54242bc88a5fa92969dcf2de2ad4a5a72
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-04-19 17:25:41 +0900

    change valiable

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from util import display_result, get_winner, display_input
+from util import display_input, display_result, get_winner
 
-value = None
-while value == None:
-  hand_list = display_input()
-  player_hand = hand_list[0]
-  opponent_hand = hand_list[1]
-  value = get_winner(player_hand, opponent_hand)
-  display_result(value)
+winner = None
+while winner == None:
+    hand_list = display_input()
+    player_hand = hand_list[0]
+    opponent_hand = hand_list[1]
+    winner = get_winner(player_hand, opponent_hand)
+    display_result(winner)
