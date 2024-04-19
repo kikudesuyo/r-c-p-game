280a8e8940ed2054561466809b57cbf4a1e556d7
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-04-19 17:31:55 +0900

    emit redundant part

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from util import display_input, display_result, get_winner
 
 winner = None
 while not winner:
-    hand_list = display_input()
-    player_hand = hand_list[0]
-    opponent_hand = hand_list[1]
+    player_hand, opponent_hand = display_input()
     winner = get_winner(player_hand, opponent_hand)
     display_result(winner)
